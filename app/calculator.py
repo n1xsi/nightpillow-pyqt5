1de542c52de8cf48e7862d016b60205a2b8bf5fe
n1xs1 <vanyalebedev45@gmail.com>
2025-08-08 17:17:48 +0300

    Edit: Refactor code

--- app/calculator.py
+++ app/calculator.py
@@ -1,13 +1,16 @@
 from datetime import datetime, timedelta
 from . import constants
 
+
 class SleepCalculator:
     """
-    Выполняет расчеты оптимального времени сна на основе циклов.
+    Выполняет расчёты оптимального времени сна на основе циклов.
     """
+
     def __init__(self):
         self.sleep_cycle = timedelta(minutes=constants.SLEEP_CYCLE_MINUTES)
-        self.fall_asleep_time = timedelta(minutes=constants.FALL_ASLEEP_MINUTES)
+        self.fall_asleep_time = timedelta(
+            minutes=constants.FALL_ASLEEP_MINUTES)
         self.num_cycles = constants.NUM_CYCLES
 
     def calculate_bed_times(self, wake_up_time_str: str) -> list[str]:
@@ -29,14 +32,14 @@ class SleepCalculator:
 
         # Считаем, что заснуть нужно за 15 минут до начала первого цикла сна
         target_sleep_time = wake_up_time - self.fall_asleep_time
-        
+
         bed_times = []
         current_time = target_sleep_time
         for _ in range(self.num_cycles):
             current_time -= self.sleep_cycle
             bed_times.append(current_time.strftime("%H:%M"))
-        
-        bed_times.reverse() # Показываем от более ранних к более поздним
+
+        bed_times.reverse()  # Показываем от более ранних к более поздним
         return bed_times
 
     def calculate_wake_up_times(self, go_to_bed_time_str: str) -> list[str]:
@@ -57,7 +60,7 @@ class SleepCalculator:
 
         # Пробуждение происходит после 15 минут засыпания и N циклов сна
         wake_up_start_time = go_to_bed_time + self.fall_asleep_time
-        
+
         wake_up_times = []
         current_time = wake_up_start_time
         for _ in range(self.num_cycles):
