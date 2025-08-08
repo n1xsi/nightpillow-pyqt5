52c5005d75217e42812be93a8ab1a1f3f0d6c25e
n1xs1 <vanyalebedev45@gmail.com>
2025-08-08 16:38:09 +0300

    Created ui constants

---
+++ app/constants.py
@@ -0,0 +1,32 @@
+# Настройки окна
+WINDOW_WIDTH = 558
+WINDOW_HEIGHT = 225
+APP_TITLE = "NightPillow"
+ICON_PATH = "resources/images/logo.ico"
+BG_PATH = "resources/images/bg.tiff"
+
+# Шрифты и цвета
+FONT_FAMILY = "Cygre"
+PRIMARY_COLOR = "#293b56"
+SECONDARY_COLOR = "#1b2739"
+TEXT_COLOR = "white"
+
+# Размеры элементов
+INPUT_SIZE = (140, 35)
+BUTTON_SIZE = (140, 35)
+TOGGLE_SIZE = (80, 50)
+
+# Логика расчёта
+SLEEP_CYCLE_MINUTES = 90
+FALL_ASLEEP_MINUTES = 15
+NUM_CYCLES = 6
+
+# UI Тексты
+TIME_LABEL = "Время:"
+TOGGLE_TEXT_WAKE_UP = "надо встать"
+TOGGLE_TEXT_GO_TO_BED = "надо лечь"
+CALCULATE_BUTTON_TEXT = "Рассчитать"
+INITIAL_RESULT_TEXT = "😴Тут будет рассчитано время!🌙"
+ERROR_TEXT = "Введите корректное время в формате ЧЧ:ММ."
+WAKE_UP_RESULT_PREFIX = "Лучше всего встать в:"
+GO_TO_BED_RESULT_PREFIX = "Лучше всего лечь спать в:"
